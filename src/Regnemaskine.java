36890081c9a61d8d4bef2a0a01c2cc0d0bfe92b6
Philip <philip.tran@hotmail.dk>
2022-09-19 14:04:31 +0100

    commit philip

--- src/Regnemaskine.java
+++ src/Regnemaskine.java
@@ -14,7 +14,6 @@ public class Regnemaskine {
         System.out.println("Tast 1 for at plusse tallene, tast 2 for at minusse tallene eller tast 3 for at gange");
         plusminus = brugerinput.nextInt();
         if (plusminus == 1) System.out.println(tal1+tal2);
-        if (plusminus == 2) System.out.println(tal1-tal2);
 
 
     }
